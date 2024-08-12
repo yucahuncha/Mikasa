e4f330192780c94598b146d468d6ebf49b7718da
长白雪 <84273011+yucahuncha@users.noreply.github.com>
2024-08-12 16:39:46 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    setWindowIcon(QIcon(":/new/logo/img/logo2.ico"));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
